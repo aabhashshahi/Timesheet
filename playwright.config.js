eac827920bf3d7cb3f28740f9823595376037b53
Aabhash14 <aabhashshahi14@gmail.com>
2026-03-05 14:09:13 +0545

    Update pipeline and test configuration

--- playwright.config.js
+++ playwright.config.js
@@ -35,7 +35,7 @@ module.exports = defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: 1,
+  workers: 6,
   reporter: [
     ['html', { open: 'never' }],
     ['json', { outputFile: 'test-results/results.json' }],
